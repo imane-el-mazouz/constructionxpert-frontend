a44d686082c5f1d2cf041d543b048a430d51ed68
imane-el-mazouz <elmazouz.imane@gmail.com>
2024-09-11 22:31:39 +0000

    edit task - management

--- construction_frontend/src/app/core/services/project.service.ts
+++ construction_frontend/src/app/core/services/project.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Project } from '../models/project.model';
 
@@ -7,15 +7,23 @@ import { Project } from '../models/project.model';
   providedIn: 'root'
 })
 export class ProjectService {
-  private apiUrl = 'http://localhost:8081/api/projects';
+  private apiUrl = 'http://localhost:8888/PROJECT-SERVICE/api/projects';
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
   getAllProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.apiUrl);
+    return this.http.get<Project[]>(this.apiUrl, { headers: this.getHeaders() });
   }
 
   deleteProject(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
   }
 }
