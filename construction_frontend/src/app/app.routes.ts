611bb3fc4e0b28c42fc3a01016e061d1090c9e7a
Nabil Sabi <nabilsabi01@gmail.com>
2024-09-11 18:02:25 +0100

    création du design des composants de la barre latérale avec mise en page réactive

--- construction_frontend/src/app/app.routes.ts
+++ construction_frontend/src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./features/auth/login/login.component";
 import {SignupComponent} from "./features/auth/signup/signup.component";
-import {AdminComponent} from "./features/dash-admin/admin-dash/admin.component";
-import {DashCustomerComponent} from "./features/dash-customer/dash-customer/dash-customer.component";
 import {CustomersManagementComponent} from "./features/dash-admin/customers-management/customers-management.component";
 import {authGuard} from "./core/guards/auth.guard";
 import {Role} from "./core/enums/role";
 import {GuardComponent} from "./features/guard/guard.component";
+import { DashUserComponent } from './features/dash-user/dash-user.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: AdminComponent,canActivate: [authGuard], data: { expectedRole: Role.ADMIN } },
-  { path: 'customer', component: DashCustomerComponent ,canActivate: [authGuard], data: { expectedRole: Role.CUSTOMER } },
+  // { path: 'dashboard', component: DashUserComponent },
+  { path: 'dashboard', component: DashUserComponent},
   { path: 'customers', component: CustomersManagementComponent ,canActivate: [authGuard], data: { expectedRole: Role.ADMIN }},
   { path: 'access-denied', component: GuardComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
