2707be76d77e6d3a12ec5a63a9668dfc5432b714
iima23 <elmazouz.imane@gmail.com>
2024-09-12 18:24:11 +0100

    add methods of resources

--- construction_frontend/src/app/app.routes.ts
+++ construction_frontend/src/app/app.routes.ts
@@ -7,16 +7,19 @@ import {Role} from "./core/enums/role";
 import {GuardComponent} from "./features/guard/guard.component";
 import {TaskManagementComponent} from "./features/dash-customer/task-management/task-management.component";
 import { DashUserComponent } from './features/dash-user/dash-user.component';
+import {ResourceManagementComponent} from "./features/dash-customer/resource-management/resource-management.component";
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  // { path: 'dashboard', component: DashUserComponent },
-  { path: 'dashboard', component: DashUserComponent},
+  {
+    path: 'dashboard', component: DashUserComponent  ,canActivate: [authGuard], data: { expectedRole: Role.CUSTOMER }
+  },
   { path: 'customers', component: CustomersManagementComponent ,canActivate: [authGuard], data: { expectedRole: Role.ADMIN }},
   { path: 'access-denied', component: GuardComponent },
   { path: 'tasks', component: TaskManagementComponent ,canActivate: [authGuard], data: { expectedRole: Role.CUSTOMER } },
+  { path: 'resources', component: ResourceManagementComponent ,canActivate: [authGuard], data: { expectedRole: Role.CUSTOMER } },
   { path: '', redirectTo: '', pathMatch: 'full' }
 
 
