7e44629852b6225b2d3e5ea1b6dfe38f933f6314
imane-el-mazouz <elmazouz.imane@gmail.com>
2024-09-12 22:32:14 +0000

    ameliore design of dash

--- construction_frontend/src/app/core/services/project.service.ts
+++ construction_frontend/src/app/core/services/project.service.ts
@@ -1,7 +1,38 @@
+// import { Injectable } from '@angular/core';
+// import {HttpClient, HttpHeaders} from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Project } from '../models/project.model';
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProjectService {
+//   private apiUrl = 'http://localhost:8888/PROJECT-SERVICE/api/projects';
+//
+//   constructor(private http: HttpClient) { }
+//
+//   private getHeaders(): HttpHeaders {
+//     const token = localStorage.getItem('token');
+//     return new HttpHeaders({
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${token}`
+//     });
+//   }
+//
+//   getAllProjects(): Observable<Project[]> {
+//     return this.http.get<Project[]>(this.apiUrl, { headers: this.getHeaders() });
+//   }
+//
+//   deleteProject(id: number): Observable<void> {
+//     return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+//   }
+// }
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { Project } from '../models/project.model';
+import {Task} from "../models/task";
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +50,41 @@ export class ProjectService {
     });
   }
 
+  createProject(project: Project): Observable<Project> {
+    return this.http.post<Project>(this.apiUrl, project, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
+  }
+
   getAllProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.apiUrl, { headers: this.getHeaders() });
+    return this.http.get<Project[]>(this.apiUrl, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
+  }
+
+  getProjectById(id: number): Observable<Project> {
+    return this.http.get<Project>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
+  }
+
+  updateProject(id: number, project: Project): Observable<Project> {
+    return this.http.put<Project>(`${this.apiUrl}/${id}`, project, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   deleteProject(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
+  existProject(id: number): Observable<boolean> {
+    return this.http.get<boolean>(`${this.apiUrl}/${id}/exist`, { headers: this.getHeaders() });
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  getProjectTasks(projectId: number): Observable<Task[]> {
+    return this.http.get<Task[]>(`/project/{projectId}`);
+  }
+
 }
--- construction_frontend/src/app/core/services/resource.service.ts
+++ construction_frontend/src/app/core/services/resource.service.ts
@@ -1,7 +1,49 @@
+// import { Injectable } from '@angular/core';
+// import {HttpClient, HttpHeaders} from "@angular/common/http";
+// import {Observable} from "rxjs";
+// import {Resource} from "../models/resource";
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ResourceService {
+//   private apiUrl = 'http://localhost:8888/RESOURCE-SERVICE/api/resources';
+//
+//   constructor(private http: HttpClient) { }
+//
+//   private getHeaders(): HttpHeaders {
+//     const token = localStorage.getItem('token');
+//     return new HttpHeaders({
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${token}`
+//     });
+//   }
+//
+//   getResources(): Observable<Resource[]> {
+//     return this.http.get<Resource[]>(this.apiUrl, { headers: this.getHeaders() });
+//   }
+//
+//   addResource(resource: Resource): Observable<Resource> {
+//     return this.http.post<Resource>(this.apiUrl, resource, { headers: this.getHeaders() });
+//   }
+//
+//   updateResource(id: number, resource: Resource): Observable<Resource> {
+//     return this.http.put<Resource>(`${this.apiUrl}/${id}`, resource, { headers: this.getHeaders() });
+//   }
+//
+//   deleteResource(id: number | undefined): Observable<void> {
+//     return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+//   }
+//
+//   getResourceById(id: number): Observable<Resource> {
+//     return this.http.get<Resource>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+//   }
+// }
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Resource} from "../models/resource";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Resource } from "../models/resource";
 
 @Injectable({
   providedIn: 'root'
@@ -20,22 +62,32 @@ export class ResourceService {
   }
 
   getResources(): Observable<Resource[]> {
-    return this.http.get<Resource[]>(this.apiUrl, { headers: this.getHeaders() });
+    return this.http.get<Resource[]>(this.apiUrl, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   addResource(resource: Resource): Observable<Resource> {
-    return this.http.post<Resource>(this.apiUrl, resource, { headers: this.getHeaders() });
+    return this.http.post<Resource>(this.apiUrl, resource, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   updateResource(id: number, resource: Resource): Observable<Resource> {
-    return this.http.put<Resource>(`${this.apiUrl}/${id}`, resource, { headers: this.getHeaders() });
+    return this.http.put<Resource>(`${this.apiUrl}/${id}`, resource, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   deleteResource(id: number | undefined): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
   }
 
   getResourceById(id: number): Observable<Resource> {
-    return this.http.get<Resource>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+    return this.http.get<Resource>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
